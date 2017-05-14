e61816032c1a47801851e95fc783d6401ff29158
jayaprakash-a <akulajayaprakash@gmail.com>
2017-05-14 18:36:11 +0530

    Added scripts for installing Opencv 3.1.0

---
+++ scripts/install_dependencies.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+wget https://repo.continuum.io/archive/Anaconda3-4.3.1-Linux-x86_64.sh
+chmod +x Anaconda3-4.3.1-Linux-x86_64.sh
+./Anacona3-4.3.1-Linux-x86_64.sh
+conda install opencv=3.1.0
