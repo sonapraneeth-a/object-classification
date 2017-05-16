e35d472208dcfe41ee3c074df1f78c6e23a37a45
jayaprakash-a <akulajayaprakash@gmail.com>
2017-05-16 22:56:11 +0530

    Changed the hot_labels to hot_fine and hot_coarse

--- library/datasets/dataset.py
+++ library/datasets/dataset.py
@@ -1,13 +1,18 @@
 class Dataset:
 
     def __init__(self, verbose=False):
+        """
+        
+        :param verbose: 
+        """
         self.verbose = verbose
         self.data = None
         self.images = None
         self.orig_data = None
         self.preprocess_data = None
         self.augment_data = None
-        self.one_hot_labels = None
+        self.one_hot_fine_labels = None
+        self.one_hot_coarse_labels = None
         self.coarse_labels = None
         self.fine_labels = None
         self.filenames = None
@@ -15,4 +20,9 @@ class Dataset:
         self.class_names = None
 
     def get_next_batch(self, batch_size=100):
+        """
+        
+        :param batch_size: 
+        :return: 
+        """
         return True
