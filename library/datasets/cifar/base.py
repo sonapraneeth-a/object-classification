a6a630a2ce4c67ae555fd830c92654d895112662
jayaprakash-a <akulajayaprakash@gmail.com>
2017-05-16 22:53:09 +0530

    Changed the classes to fine_classes

--- library/datasets/cifar/base.py
+++ library/datasets/cifar/base.py
@@ -20,6 +20,20 @@ class CIFARBase:
                  image_mode='rgb',
                  save_h5py=True,
                  verbose=False):
+        """
+        
+        :param num_images: 
+        :param one_hot_encode: 
+        :param train_validate_split: 
+        :param preprocess: 
+        :param augment: 
+        :param num_test_images: 
+        :param endian: 
+        :param make_image: 
+        :param image_mode: 
+        :param save_h5py: 
+        :param verbose: 
+        """
         self.verbose = verbose
         self.img_height = 32
         self.img_width = 32
@@ -48,21 +62,41 @@ class CIFARBase:
         self.preprocess = preprocess
         self.augment = augment
         self.save_h5py = save_h5py
-        self.classes = None
-        self.num_classes = None
+        self.fine_classes = None
+        self.coarse_classes = None
+        self.num_fine_classes = None
+        self.num_coarse_classes = None
         self.file_url = None
         self.file_md5 = None
 
     def get_image_width(self):
+        """
+        
+        :return: 
+        """
         return self.img_width
 
     def get_image_height(self):
+        """
+        
+        :return: 
+        """
         return self.img_height
 
     def get_num_image_channels(self):
+        """
+        
+        :return: 
+        """
         return self.img_channels
 
     def convert_images(self, raw, type='rgb'):
+        """
+        
+        :param raw: 
+        :param type: 
+        :return: 
+        """
         raw_float = np.array(raw, dtype=float)
         if type == 'rgb_float':
             # Convert the raw images from the data-files to floating-points.
@@ -83,58 +117,129 @@ class CIFARBase:
             images_n = images_n / 255.0
         return images_n
 
-    def convert_one_hot_encoding(self, classes, data_type='train'):
+    def convert_one_hot_encoding(self, classes, data_type='train', class_type='fine'):
+        """
+        
+        :param classes: 
+        :param data_type: 
+        :param class_type: 
+        :return: 
+        """
         num_classes = np.max(classes) + 1
-        if data_type == 'train':
-            self.train.one_hot_labels = np.zeros((classes.shape[0], num_classes))
-        if data_type == 'validate':
-            self.validate.one_hot_labels = np.zeros((classes.shape[0], num_classes))
-        if data_type == 'test':
-            self.test.one_hot_labels = np.zeros((classes.shape[0], num_classes))
+        if data_type == 'train' and class_type == 'fine':
+            self.train.one_hot_fine_labels = np.zeros((classes.shape[0], num_classes))
+        elif data_type == 'train' and class_type == 'coarse':
+            self.train.one_hot_coarse_labels = np.zeros((classes.shape[0], num_classes))
+        if data_type == 'validate' and class_type == 'fine':
+            self.validate.one_hot_fine_labels = np.zeros((classes.shape[0], num_classes))
+        elif data_type == 'validate' and class_type == 'coarse':
+            self.validate.one_hot_coarse_labels = np.zeros((classes.shape[0], num_classes))
+        if data_type == 'test' and class_type == 'fine':
+            self.test.one_hot_fine_labels = np.zeros((classes.shape[0], num_classes))
+        elif data_type == 'test' and class_type == 'coarse':
+            self.test.one_hot_coarse_labels = np.zeros((classes.shape[0], num_classes))
+
         for i in range(classes.shape[0]):
-            if self.endian == 'big':
+            if self.endian == 'big' and class_type == 'fine':
                 if data_type == 'train':
-                    self.train.one_hot_labels[i, num_classes - 1 - classes[i]] = 1
+                    self.train.one_hot_fine_labels[i, num_classes - 1 - classes[i]] = 1
                 if data_type == 'validate':
-                    self.validate.one_hot_labels[i, num_classes - 1 - classes[i]] = 1
+                    self.validate.one_hot_fine_labels[i, num_classes - 1 - classes[i]] = 1
                 if data_type == 'test':
-                    self.test.one_hot_labels[i, num_classes-1-classes[i]] = 1
-            if self.endian == 'little':
+                    self.test.one_hot_fine_labels[i, num_classes-1-classes[i]] = 1
+            elif self.endian == 'big' and class_type == 'coarse':
                 if data_type == 'train':
-                    self.train.one_hot_labels[i, classes[i]] = 1
+                    self.train.one_hot_coarse_labels[i, num_classes - 1 - classes[i]] = 1
                 if data_type == 'validate':
-                    self.validate.one_hot_labels[i, classes[i]] = 1
+                    self.validate.one_hot_coarse_labels[i, num_classes - 1 - classes[i]] = 1
                 if data_type == 'test':
-                    self.test.one_hot_labels[i, classes[i]] = 1
+                    self.test.one_hot_coarse_labels[i, num_classes-1-classes[i]] = 1
+            if self.endian == 'little' and class_type == 'fine':
+                if data_type == 'train':
+                    self.train.one_hot_fine_labels[i, classes[i]] = 1
+                if data_type == 'validate':
+                    self.validate.one_hot_fine_labels[i, classes[i]] = 1
+                if data_type == 'test':
+                    self.test.one_hot_fine_labels[i, classes[i]] = 1
+            elif self.endian == 'little' and class_type == 'coarse':
+                if data_type == 'train':
+                    self.train.one_hot_coarse_labels[i, classes[i]] = 1
+                if data_type == 'validate':
+                    self.validate.one_hot_coarse_labels[i, classes[i]] = 1
+                if data_type == 'test':
+                    self.test.one_hot_coarse_labels[i, classes[i]] = 1
 
     def train_images(self):
+        """
+        
+        :return: 
+        """
         return self.train.data
 
     def train_labels(self):
+        """
+        
+        :return: 
+        """
         return self.train.one_hot_labels
 
     def train_classes(self):
-        return self.train.class_labels
+        """
+        
+        :return: 
+        """
+        return self.train.fine_labels
 
     def validate_images(self):
+        """
+        
+        :return: 
+        """
         return self.validate.data
 
     def validate_labels(self):
+        """
+        
+        :return: 
+        """
         return self.validate.one_hot_labels
 
     def validate_classes(self):
-        return self.validate.class_labels
+        """
+        
+        :return: 
+        """
+        return self.validate.fine_labels
 
     def test_images(self):
+        """
+        
+        :return: 
+        """
         return self.test.data
 
     def test_labels(self):
+        """
+        
+        :return: 
+        """
         return self.test.one_hot_labels
 
     def test_classes(self):
-        return self.test.class_labels
+        """
+        
+        :return: 
+        """
+        return self.test.fine_labels
 
     def plot(self, grid, matrix, fontsize=10):
+        """
+        
+        :param grid: 
+        :param matrix: 
+        :param fontsize: 
+        :return: 
+        """
         k = 0
         class_type = 0
         for ax in grid:
@@ -150,7 +255,7 @@ class CIFARBase:
             ax.set_xlim([0, 32])
             ax.set_ylim([32, 0])
             if k % 10 == 0:
-                ax.set_ylabel(self.classes[class_type], rotation=0, ha='right',
+                ax.set_ylabel(self.fine_classes[class_type], rotation=0, ha='right',
                               weight='bold', size=fontsize)
                 class_type += 1
             k += 1
@@ -159,6 +264,16 @@ class CIFARBase:
 
     def plot_sample(self, plot_train=True, plot_test=False, verbose=False, fig_size=(7, 7), fontsize=12,
                     images_per_class=10):
+        """
+        
+        :param plot_train: 
+        :param plot_test: 
+        :param verbose: 
+        :param fig_size: 
+        :param fontsize: 
+        :param images_per_class: 
+        :return: 
+        """
         num_images_per_class = images_per_class
         if self.train.data is None and plot_test is False:
             self.load_data(train=plot_train, test=plot_test)
@@ -169,10 +284,10 @@ class CIFARBase:
         data_image_nos = []
         test_image_nos = []
         if plot_train is True:
-            for class_type in range(self.num_classes):
-                data_class_labels = np.where(self.train.class_labels == class_type)
-                data_class_labels = data_class_labels[0][:num_images_per_class].tolist()
-                data_image_nos.extend(data_class_labels)
+            for class_type in range(self.num_fine_classes):
+                data_fine_labels = np.where(self.train.fine_labels == class_type)
+                data_fine_labels = data_fine_labels[0][:num_images_per_class].tolist()
+                data_image_nos.extend(data_fine_labels)
             example_data_images = self.train.data[data_image_nos, :]
             example_data_image_matrix = []
             k = 0
@@ -181,11 +296,11 @@ class CIFARBase:
                 k += 1
             example_data_image_matrix = np.array(example_data_image_matrix)
         if plot_test is True:
-            for class_type in range(self.num_classes):
-                test_class_labels = np.where(self.test.class_labels == class_type)
-                test_class_labels = test_class_labels[0][:num_images_per_class]
-                test_class_labels = test_class_labels.tolist()
-                test_image_nos.extend(test_class_labels)
+            for class_type in range(self.num_fine_classes):
+                test_fine_labels = np.where(self.test.fine_labels == class_type)
+                test_fine_labels = test_fine_labels[0][:num_images_per_class]
+                test_fine_labels = test_fine_labels.tolist()
+                test_image_nos.extend(test_fine_labels)
             example_test_images = self.test.data[test_image_nos, :]
             example_test_image_matrix = []
             k = 0
@@ -193,14 +308,13 @@ class CIFARBase:
                 example_test_image_matrix.append(self.convert_images(example_test_images[k, :]))
                 k += 1
             example_test_image_matrix = np.array(example_test_image_matrix)
-        num_rows = 10
+        num_rows = min(self.num_fine_classes, 20)
         num_cols = num_images_per_class
         if verbose is True:
             print('Plot image matrix shape: ' + str(example_data_image_matrix.shape))
             print('Number of rows: %d' % num_rows)
             print('Number of cols: %d' % num_cols)
         if plot_train is True:
-            print('Plotting CIFAR 10 Train Dataset')
             data_fig = plt.figure()
             data_fig.set_figheight(fig_size[0])
             data_fig.set_figwidth(fig_size[1])
@@ -209,7 +323,6 @@ class CIFARBase:
                              )
             self.plot(data_grid, example_data_image_matrix, fontsize=fontsize)
         if plot_test is True:
-            print('Plotting CIFAR 10 Test Dataset')
             test_fig = plt.figure()
             test_fig.set_figheight(fig_size[0])
             test_fig.set_figwidth(fig_size[1])
@@ -218,9 +331,24 @@ class CIFARBase:
                              )
             self.plot(test_grid, example_test_image_matrix, fontsize=fontsize)
 
-    def plot_images(self, images, cls_true, cls_pred=None, nrows=3, ncols=3, fig_size=(7,7),
-                    fontsize=15, convert=False, type='rgb'):
-        assert images.shape[0] == cls_true.shape[0]
+    def plot_images(self, images, cls_true_fine, cls_true_coarse=None, cls_pred_fine=None, cls_pred_coarse=None,
+                    nrows=3, ncols=3, fig_size=(7, 7), fontsize=15, convert=False, type='rgb'):
+        """
+        
+        :param images: 
+        :param cls_true_fine: 
+        :param cls_true_coarse: 
+        :param cls_pred_fine: 
+        :param cls_pred_coarse: 
+        :param nrows: 
+        :param ncols: 
+        :param fig_size: 
+        :param fontsize: 
+        :param convert: 
+        :param type: 
+        :return: 
+        """
+        assert images.shape[0] == cls_true_fine.shape[0]
         fig, axes = plt.subplots(nrows, ncols)
         if fig_size is not None:
             fig.set_figheight(fig_size[0])
@@ -237,10 +365,23 @@ class CIFARBase:
             if type == 'grey':
                 ax.imshow(toimage(image), cmap=matplotlib.cm.Greys_r)
             # Show true and predicted classes.
-            if cls_pred is None:
-                xlabel = "True: {0}".format(cls_true[image_no])
+            if cls_pred_fine is None and cls_pred_coarse is None:
+                if cls_true_coarse is None:
+                    xlabel = "Fine: {0}".format(cls_true_fine[image_no])
+                else:
+                    xlabel = "Fine: {0}\nCoarse: {1}".format(cls_true_fine[image_no], cls_true_coarse[image_no])
             else:
-                xlabel = "True: {0}\nPred: {1}".format(cls_true[image_no], cls_pred[image_no])
+                if cls_true_coarse is None:
+                    if cls_pred_fine is None:
+                        xlabel = "Fine: {0}".format(cls_true_fine[image_no])
+                    else:
+                        xlabel = "Fine: {0}\nPred. Fine: {1}".format(cls_true_fine[image_no], cls_pred_fine[image_no])
+                else:
+                    if cls_pred_coarse is None:
+                        xlabel = "Coarse: {0}".format(cls_true_coarse[image_no])
+                    else:
+                        xlabel = "Coarse: {0}\nPred. Coarse: {1}".format(cls_true_coarse[image_no],
+                                                                         cls_pred_coarse[image_no])
             ax.set_xlabel(xlabel, weight='bold', size=fontsize)
             # Remove ticks from the plot.
             ax.set_xticks([])
