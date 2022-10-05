f27bb52889244c1745591568f727d536416eb4c2
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-05 01:13:11 +0000

    Adding numeric_owner as keyword arguement

--- library/utils/file_utils.py
+++ library/utils/file_utils.py
@@ -182,7 +182,7 @@ def untar(source_filename, destination_dir='.', verbose=False):
                     if not is_within_directory(path, member_path):
                         raise Exception("Attempted Path Traversal in Tar File")
             
-                tar.extractall(path, members, numeric_owner) 
+                tar.extractall(path, members, numeric_owner=numeric_owner) 
                 
             
             safe_extract(tar, destination_dir)
